d241bf0c9ba5ddb8d81a63ad98eaacb73db1e401
Jeffrey-Hying <jeffrey@hying.com.tw>
2024-04-24 17:51:00 +0800

    add simple flask get json stockData

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+from flask import Flask, request, jsonify
 import shioaji as sj
 from dotenv import load_dotenv
 import os
+
 # 載入 .env 文件中的變數
 load_dotenv()
 
@@ -11,3 +13,12 @@ api.login(
 )
 stock1 = api.Contracts.Stocks["2890"]
 print(stock1)
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_stock1():
+    return jsonify({"stock1": str(stock1)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
